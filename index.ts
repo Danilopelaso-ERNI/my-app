c708f3574a05c0700c00eb9ce726ac95e3c4fc80
Danilopelaso-ERNI <danilo.pelaso@betterask.erni>
2025-02-11 16:38:32 +0800

    first commit

---
+++ index.ts
@@ -0,0 +1,13 @@
+// console.log(typeof 40);
+
+const abs = () => {
+    console.log(abc.xyz);
+    
+}
+
+abc()
+abc.xyz = 400
+abc.xyz = 200
+abc()
+
+
---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+const App = () => {
+
+  const [color, setColor] = useState('red')
+
+  const changeColor = (newColor) => {
+    setColor(newColor)
+  }
+
+  return (
+    <div className="w-full h-screen duration-200 bg-green-800" style={{backgroundColor: color}}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-2 py-2 rounded-3xl">
+          <button onClick={() => changeColor('green')} className="outline-none px-4 py-1 rounded-full text-black shadow-lg">test</button>
+          <button className="outline-none px-4 py-1 rounded-full text-black shadow-lg">test</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
